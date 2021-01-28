8a7d878f6fe643943bf763130866b926a6fdcb42
Daniel Medina Garate <43603778+dmedinag29@users.noreply.github.com>
2021-01-27 21:52:33 -0800

    Add files via upload

---
+++ 7. Dynamic Linking/randmain.mk
@@ -0,0 +1,12 @@
+randmain: randlibsw.so randlibhw.so
+	$(CC) $(CFLAGS) -c randcpuid.c -o randcpuid.o
+	$(CC) $(CFLAGS) -c randmain.c -o randmain.o
+	$(CC) $(CFLAGS) -ldl -Wl,-rpath=$(PWD) randcpuid.o randmain.o -o $@
+
+randlibsw.so: randlibsw.c
+	$(CC) $(CFLAGS) -fPIC -c randlibsw.c -o randlibsw.o
+	$(CC) $(CFLAGS) -shared randlibsw.o -o $@
+
+randlibhw.so: randlibhw.c
+	$(CC) $(CFLAGS) -fPIC -c randlibhw.c -o randlibhw.o
+	$(CC) $(CFLAGS) -shared randlibhw.o -o $@
